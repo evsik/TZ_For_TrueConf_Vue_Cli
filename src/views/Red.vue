ee936e72a7784392dc714ac60bc467841c1882e9
evsik <evsevsikov@gmail.com>
2021-03-25 16:47:07 +0300

    finish project on vueCLI

--- src/views/Red.vue
+++ src/views/Red.vue
@@ -2,6 +2,7 @@
   <div>
     <TrafficLights></TrafficLights>
     <Timer></Timer>
+
   </div>
 </template>
 
@@ -14,9 +15,7 @@ export default {
 
   data() {
     return {
-      time: 10,
-      setStorage: localStorage.setItem('test', "6"),
-      getStorage: localStorage.getItem('test')
+      time: 10
     }
   },
   components: {
@@ -31,32 +30,9 @@ export default {
       color: document.querySelector("[color=red]"),
       class: "red"
     })
-    // this.onTrafficLights(0)
   },
 
   methods: {
-    // onTrafficLights(to) {
-    //   let current = localStorage.getItem('test')
-    //   current = +current
-    //   let path = document.getElementById("countdown")
-    //   let light = document.querySelector("[color=red]")
-    //
-    //   light.classList.add("red")
-    //
-    //   let timerId = setInterval(function () {
-    //
-    //     path.innerHTML = current
-    //     console.log(current)
-    //
-    //     if (current == to) {
-    //       clearInterval(timerId)
-    //     } else if (this.time <= 3) {
-    //       light.classList.toggle("red")
-    //     }
-    //     current--
-    //     return localStorage.setItem('test', current)
-    //   }, 1000)
-    // },
   },
 
 }
